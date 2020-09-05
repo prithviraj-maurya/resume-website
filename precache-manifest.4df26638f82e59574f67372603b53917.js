6434f8eef9860a27e2be40eff92ee3484f656332
Prithviraj <mouryaprithviraj@gmail.com>
2020-09-05 22:42:54 +0530

    Updates

---
+++ precache-manifest.4df26638f82e59574f67372603b53917.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f329cb556359c844ee80f6d2853a297",
+    "url": "/resume-website/index.html"
+  },
+  {
+    "revision": "df28e57f3aef4204c153",
+    "url": "/resume-website/static/css/2.1f0aa724.chunk.css"
+  },
+  {
+    "revision": "f18439b58df184ebf646",
+    "url": "/resume-website/static/css/main.2ce37816.chunk.css"
+  },
+  {
+    "revision": "df28e57f3aef4204c153",
+    "url": "/resume-website/static/js/2.f1d1c63c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume-website/static/js/2.f1d1c63c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f18439b58df184ebf646",
+    "url": "/resume-website/static/js/main.7983cbb1.chunk.js"
+  },
+  {
+    "revision": "c4c7419b7edca97c7079",
+    "url": "/resume-website/static/js/runtime-main.0d2b2e5b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/resume-website/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/resume-website/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/resume-website/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/resume-website/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/resume-website/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
