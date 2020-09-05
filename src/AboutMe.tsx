058032363f210316d6b557c41a79a12a1ca1ec8d
Prithviraj <mouryaprithviraj@gmail.com>
2020-09-06 02:00:02 +0530

    adding home page

--- src/AboutMe.tsx
+++ src/AboutMe.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 class AboutMe extends React.Component {
     render() {
         return (
-            <div>About Me</div>
+            <div className="container about">
+                <h1 className="title"><b>ABOUT ME</b></h1>
+                <hr className="hr-dark"/><hr className="hr-light"/>
+            </div>
         );
     }
 }
--- src/Resume.tsx
+++ src/Resume.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Style.css';
+import profile from './static/Profile.jpg';
 import StarfieldAnimation from 'react-starfield-animation';
 
 import AboutMe from './AboutMe';
 
 class Resume extends React.Component {
+
+  redirectLinkedIn(e) {
+    e.preventDefault();
+    window.open("http://www.linkedin.com/in/prithviraj-maurya-02aa40116");
+  }
+
+  redirectGithub(e) {
+    e.preventDefault();
+    window.open("http://github.com/prithviraj-maurya");
+  }
+
+  redirectKaggle(e) {
+    e.preventDefault();
+    window.open("http://www.kaggle.com/prithviraj7387");
+  }
+
   render() {
     return (
       <div>
@@ -36,8 +53,22 @@ class Resume extends React.Component {
                               }}
                             />
                             <div className="home">
-                              <div className="title">Hi, I am Prithviraj Maurya</div>
-                              <span className="hint">Full Stack Developer</span>
+                              <div className="profile-pic">
+                                <img className="profile-img" src={profile} alt="Profile"/>
+                              </div>
+                              <div className="title">Hi, I am <span className="name">Prithviraj Maurya</span></div>
+                              <span className="hint">Full Stack Developer, Active Kaggler, AI ML Enthusiast</span>
+                              <div className="social">
+                                <a href="/#" onClick={this.redirectLinkedIn}>
+                                  <i className="fa fa-linkedin" aria-hidden="true"></i>
+                                </a>
+                                <a href="/#" onClick={this.redirectGithub}>
+                                  <i className="fa fa-github" aria-hidden="true"></i>
+                                </a>
+                                <a href="/#" onClick={this.redirectKaggle}>
+                                  <span style={{color: 'white'}}>k</span>
+                                </a>
+                                </div>
                             </div>
                         </div>
                         <div className="page" id="p2">
